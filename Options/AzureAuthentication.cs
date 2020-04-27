6967a9d67bccca0a663b10bcb1eff961a05edbe7
Savpek <savolainen.pekka@gmail.com>
2020-04-27 13:05:53 +0300

    Cleanup for obsolete options attribute

--- Options/AzureAuthentication.cs
+++ Options/AzureAuthentication.cs
@@ -5,8 +5,6 @@ namespace Container.Updater.Options
     public class AzureAuthentication
     {
         public bool ManualAuthentication { get; set; }
-
-        [Required(AllowEmptyStrings = false)]
         public string SubscriptionId { get; set; }
         public string TenantId { get; set; }
         public string ClientId { get; set; }
